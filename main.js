7e2b71e6a2d1d8bbd7450004b39322dbf0d10388
David Corbin <daconex@gmail.com>
2016-07-18 18:53:24 -0400

    Show a red X badge on the chrome icon when Productivity desktop application is not running

--- main.js
+++ main.js
@@ -9,14 +9,28 @@
     // Send data to desktop app
     function send(data) {
         var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = parseresponse(xhr);
-        xhr.open("POST", "http://localhost:8000/productivity", true);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == XMLHttpRequest.DONE) {
+                parseresponse(xhr.status);
+            }
+        };
+        xhr.open("POST", "http://127.0.0.1:8000/productivity", true);
         xhr.send(data);
     }
 
     // Parse data returned from application after http request
-    function parseresponse(xhr) {
-        //console.log(xhr);
+    function parseresponse(httpstatus) {
+        // If http request is not successful, show an X next to the icon
+        if (httpstatus!=200) {
+            chrome.browserAction.setBadgeText({
+                "text":"X"
+            });
+        }
+        else {
+            chrome.browserAction.setBadgeText({
+                "text":""
+            });
+        }
     }
 
     // Get user agent
@@ -29,44 +43,38 @@
         send('{"status":"running", "source":"' + getUA() + '"}');
     }
 
+    // Generate array from tab data
+    function getTabData(status, url, title) {
+        var data = {};
+        data.status = status;
+        data.url = url;
+        data.title = title;
+        data.browser = "Firefox";
+        return data;
+    }
+
+    // Previous url sent
+    var prevUrl = "";
+
     // Called when tab is updated
     chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
         if (changeInfo.status == 'complete') {
-
-            // Don't send message if new tab
-            if (tab.url=="chrome://newtab/") {
-                return;
-            }
-
-            var tabdata = {};
-            tabdata.status = "tab_updated";
-            tabdata.url = tab.url;
-            tabdata.title = tab.title;
-            tabdata.favicon = tab.favIconUrl;
-            console.log(tabdata);
+            var tabdata = getTabData("tab_updated", tab.url, tab.title);
             send(JSON.stringify(tabdata));
+            prevUrl = tab.url;
         }
     });
 
     // Called when tabs are switched
     chrome.tabs.onActivated.addListener(function(tab) {
         chrome.tabs.get(tab.tabId, function(tab) {
-
-            // Don't send message if new tab
-            if (tab.url=="chrome://newtab/") {
-                return;
-            }
-
-            var tabdata = {};
-            tabdata.status = "tab_switched";
-            tabdata.url = tab.url;
-            tabdata.title = tab.title;
-            tabdata.favicon = tab.favIconUrl;
-            console.log(tabdata);
+            var tabdata = getTabData("tab_switched", tab.url, tab.title);
             send(JSON.stringify(tabdata));
+            prevUrl = tab.url;
         });
     });
 
+    // Send connection message when extension starts
     connect();
 
 })();
